580dda93c88909f272cca151b12e8680f4e55ada
Silicon42 <CurtisStofer@gmail.com>
2025-02-04 23:05:31 -0800

    added giscus comment functionality to blog pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,7 +1,8 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
-
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
@@ -37,9 +38,9 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
+            editUrl:
             'https://github.com/Silicon42/Silicon42.github.io/edit/main/',
         },
         blog: {
@@ -48,8 +49,8 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           editUrl:
             'https://github.com/Silicon42/Silicon42.github.io/edit/main/',
           // Useful options to enforce blogging best practices
@@ -63,7 +64,15 @@ const config: Config = {
       } satisfies Preset.Options,
     ],
   ],
-
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
   themeConfig: {
     // Replace with your project's social card
     //image: 'img/docusaurus-social-card.jpg',
